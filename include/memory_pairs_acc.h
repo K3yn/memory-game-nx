423184aa810e238b1b3588c466fa6571253ba06f
K3yn <44470230+K3yn@users.noreply.github.com>
2018-11-04 17:51:24 +0100

    Create memory_pairs_acc.h

---
+++ include/memory_pairs_acc.h
@@ -0,0 +1,8 @@
+#ifndef memory_pairs_acc_H
+#define memory_pairs_acc_H
+char errorAccount[100];
+char username[0x21];//current account playing
+bool userAnonymous;
+char dbgRC[2];
+void getUserInfo();
+#endif
