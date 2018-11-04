66ee6059ffb457ef997fb4f8fde9717e48322f41
K3yn <44470230+K3yn@users.noreply.github.com>
2018-11-04 17:38:16 +0100

    Create memory_pairs_touch.c

---
+++ source/memory_pairs_touch.c
@@ -0,0 +1,17 @@
+#include <switch.h>
+#include "memory_pairs_touch.h"
+
+bool inBox(touchPosition touch, int x1, int y1, int x2, int y2)
+{
+	int tx=touch.px;
+	int ty=touch.py;
+
+	if (tx > x1 && tx < x2 && ty > y1 && ty < y2)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
