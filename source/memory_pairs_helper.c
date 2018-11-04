fb1e054273b96cfe6df1b93dd7926c652971b1fd
K3yn <44470230+K3yn@users.noreply.github.com>
2018-11-04 17:37:01 +0100

    Update memory_pairs_helper.c

--- source/memory_pairs_helper.c
+++ source/memory_pairs_helper.c
@@ -1,6 +1,6 @@
-#include <SDL2/SDL.h>
+//#include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-#include <SDL2/SDL_mixer.h>
+//#include <SDL2/SDL_mixer.h>
 #include <SDL2/SDL_ttf.h>
 #include "SDL_helper.h"
 #include "memory_pairs_touch.h"
