ba797e2d00e522bdfc3a4ce8b31233e66615ea9b
K3yn <44470230+K3yn@users.noreply.github.com>
2018-11-04 17:21:34 +0100

    Create memory_pairs_fonts.c

---
+++ source/memory_pairs_fonts.c
@@ -0,0 +1,16 @@
+#include <switch.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+
+#include "memory_pairs_fonts.h"
+
+//
+void load_fonts()
+{
+	Roboto = TTF_OpenFont("romfs:/resources/Roboto-Regular.ttf", 25);
+	Roboto_large = TTF_OpenFont("romfs:/resources/Roboto-Regular.ttf", 35);
+	Roboto_50 = TTF_OpenFont("romfs:/resources/Roboto-Regular.ttf", 50);
+	Roboto_60 = TTF_OpenFont("romfs:/resources/Roboto-Regular.ttf", 60);
+	Roboto_80 = TTF_OpenFont("romfs:/resources/Roboto-Regular.ttf", 80);
+	Roboto_max = TTF_OpenFont("romfs:/resources/Roboto-Regular.ttf", 100);
+}
